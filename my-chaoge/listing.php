ab925c6d29fa169194b465be4a85d6f3909d1d66
yyli12 <yyli12@fudan.edu.cn>
2015-07-20 15:32:26 +0800

    identical to the origin version, wait to rewrite

---
+++ my-chaoge/listing.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('Kijiji.php');
+
+print "<p><a href=index.php>回主页</a></p>";
+
+$id = @$_GET['id'];
+$c = new Category();
+$c->id = $id ? $id : 2101;
+
+$c->load();
+
+
+print "<h1>$c->name</h1><p>";
+
+foreach ($c->toRoot() as $cc) {
+	print "<a href=listing.php?id={$cc->id}>{$cc->name}</a>|";
+}
+
+print "<p>";
+
+foreach ($c->children() as $cc) {
+	print "<a href=listing.php?id={$cc->id}>{$cc->name}</a>|";
+}
+
+print "<p>";
+
+foreach ($c->ads() as $ad) {
+	print "<li><a href=view.php?id={$ad->id}>{$ad->name}</a>";
+}
+
+?>
